a8cc2d17ca5c9120befd35e886faeccbdb88bf68
Francisco Yirá Albornoz <francisco.yira@outlook.com>
2022-03-10 20:12:01 -0300

    retrieving replies, the hacky way - step 1

--- source/retrieving_replies.R
+++ source/retrieving_replies.R
@@ -7,4 +7,10 @@ library(reticulate)
 
 tweets_for_plot2 <- read_rds(here("cache", "tweets_for_plot2.rds"))
 
-
+# The hacky way: getting followers or Boric and Kast
+boric_followers <- xfun::cache_rds({
+  rtweet::get_followers("gabrielboric",
+                        n = 1600000,
+                        retryonratelimit = TRUE)
+},
+file = "boric_followers.rds")
